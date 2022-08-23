353e062a6ab7d2b11ca61c132d4efe374eb494e9
Al West <al@whatbook.com>
2022-08-23 11:19:04 +0100

    Fix extra bracket

--- Day 4/main.go
+++ Day 4/main.go
@@ -61,8 +61,7 @@ func main() {
 	fmt.Printf("Part2: %d\n", part2(numbers, boards))
 
 	numbers = []string{"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1"}
-	boards = [][]string{[]string{"22", "13", "17", "11", "0", "8", "2", "23", "4", "24", "21", "9", "14", "16", "7", "6", "10", "3", "18", "5", "1", "12", "20", "15", "19"}, []string{"3", "15", "0", "2", "22", "9", "18", "13", "17", "5", "19", "8", "7", "25", "23", "20", "11", "10","24", "4", "14", "21", "16", "12", "6"}, []string{"14", "21", "17", "24", "4", "10", "16", "15", "9", "19", "18", "8", "23", "26", "20", "22", "11", "13", "6", "5", "2", "0", "12", "3", "7"}})
-
+	boards = [][]string{[]string{"22", "13", "17", "11", "0", "8", "2", "23", "4", "24", "21", "9", "14", "16", "7", "6", "10", "3", "18", "5", "1", "12", "20", "15", "19"}, []string{"3", "15", "0", "2", "22", "9", "18", "13", "17", "5", "19", "8", "7", "25", "23", "20", "11", "10", "24", "4", "14", "21", "16", "12", "6"}, []string{"14", "21", "17", "24", "4", "10", "16", "15", "9", "19", "18", "8", "23", "26", "20", "22", "11", "13", "6", "5", "2", "0", "12", "3", "7"}}
 
 	fmt.Printf("PartX %d\n", part1(numbers, boards))
 }
@@ -107,7 +106,7 @@ func part2(numbers []string, boards [][]string) int {
 		result = checkBoards(boards)
 		if len(result) != 0 {
 			for i := range result {
-				br, _ := strconv.Atoi(result[len(result) - 1 -i])
+				br, _ := strconv.Atoi(result[len(result)-1-i])
 				boards = removeBoard(boards, br)
 			}
 		}
@@ -139,7 +138,7 @@ func calculateAnswer(board []string, number string) int {
 	return total * num
 }
 
-func makeRange (num int) []string {
+func makeRange(num int) []string {
 	var intRange []string
 	for i := 0; i < num; i++ {
 		val := strconv.Itoa(i + 1)
